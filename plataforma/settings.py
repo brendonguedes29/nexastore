786622083b38e411545d529363bb1d839b74f492
brendonguedes29 <bsg181818@gmail.com>
2026-04-08 00:33:32 -0300

    corrige settings render

--- plataforma/settings.py
+++ plataforma/settings.py
@@ -156,10 +156,11 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # =========================================
 # E-MAIL
 # =========================================
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "smtp.gmail.com"
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER", "bsg181818@gmail.com")
-EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD", "dlrpbrhjmzsexvls")
+EMAIL_HOST_USER = 'bsg181818@gmail.com'
+EMAIL_HOST_PASSWORD = 'dlrpbrhjmzsexvls'
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
