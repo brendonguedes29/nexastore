8f103a41f38712dc7e74d2f9f3ac1399e9967ab7
brendonguedes29 <bsg181818@gmail.com>
2026-04-08 21:44:39 -0300

    desativa smtp temporariamente

--- plataforma/settings.py
+++ plataforma/settings.py
@@ -157,7 +157,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # E-MAIL
 # =========================================
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
