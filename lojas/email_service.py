6bde67f7a74633826dc30a8969c9d66cbf41fd63
brendonguedes29 <bsg181818@gmail.com>
2026-04-11 19:24:10 -0300

    fix resend

--- lojas/email_service.py
+++ lojas/email_service.py
@@ -7,7 +7,7 @@ resend.api_key = settings.RESEND_API_KEY
 def enviar_email(destinatario, assunto, html):
     try:
         resend.Emails.send({
-            "from": "NexaStore <noreply@nexastore.resend.dev>",
+            "from": "bsg181818@gmail.com",
             "to": [destinatario],
             "subject": assunto,
             "html": html,
