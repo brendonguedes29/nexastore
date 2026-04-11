284d9995198c7ae150ff105f0001131b08b3e0e9
brendonguedes29 <bsg181818@gmail.com>
2026-04-10 23:21:19 -0300

    fix smtp email

--- plataforma/settings.py
+++ plataforma/settings.py
@@ -172,7 +172,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # =========================================
 # EMAIL
 # =========================================
-EMAIL_BACKEND = "django.core.mail.backends.smpt.EmailBackend"
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
