424d591bd45e43a02ae77cd542de7a7ffc77f4f7
brendonguedes29 <bsg181818@gmail.com>
2026-04-17 11:46:01 -0300

    fix webhook mercadopago

--- plataforma/middleware.py
+++ plataforma/middleware.py
@@ -1,14 +1,15 @@
-from lojas.models import Loja
-
-class LojaMiddleware:
+class SubdominioMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
-        host = request.get_host().split(":")[0]
+        host = request.get_host().split(':')[0]
 
-        loja = Loja.objects.filter(dominio=host).first()
-
-        request.loja = loja
+        # ignora domínio principal
+        if host == "nexastoreofficial.com.br" or host.startswith("www."):
+            request.loja = None
+        else:
+            from lojas.models import Loja
+            request.loja = Loja.objects.filter(dominio=host).first()
 
         return self.get_response(request)
