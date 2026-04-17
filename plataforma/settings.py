9da992b10308d03ab3601202b117c62b4e229808
brendonguedes29 <bsg181818@gmail.com>
2026-04-17 11:35:45 -0300

    fix webhook mercadopago

--- plataforma/settings.py
+++ plataforma/settings.py
@@ -87,7 +87,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "plataforma.middleware.LojaMiddleware",
+    'plataforma.middleware.SubdominioMiddleware'
 ]
 
 # =========================================
