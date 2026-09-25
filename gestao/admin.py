14584a790955526a8bc728ac560e4bc61ab7732e
brendonguedes29 <bsg181818@gmail.com>
2026-09-24 23:20:08 -0300

    Delete gestao directory

--- gestao/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import *
-for m in [Processo,Indicador,MedicaoIndicador,PlanoAcao,NaoConformidade,DocumentoGestao,Auditoria,RequisitoISO,RiscoFMEA,RegistroProducao,IdeiaMelhoria,Setor,Colaborador,Tarefa,NotaWorkspace,ProjetoQualidade,Treinamento,TrilhaColaborador,TentativaJogo,RegistroLGPD]:
-    try: admin.site.register(m)
-    except admin.sites.AlreadyRegistered: pass
