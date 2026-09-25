181822f76da3031fa6326aa930ddd18372274d9b
brendonguedes29 <bsg181818@gmail.com>
2026-09-25 12:22:29 -0300

    Delete gestao directory

--- gestao/management/commands/processar_alertas.py
+++
@@ -1,7 +0,0 @@
-from django.core.management.base import BaseCommand
-from gestao.services.alertas import processar_alertas
-class Command(BaseCommand):
-    help='Gera avisos de vencimento na plataforma e por e-mail via Brevo.'
-    def handle(self,*args,**opts):
-        total=processar_alertas()
-        self.stdout.write(self.style.SUCCESS(f'{total} novo(s) alerta(s) criado(s).'))
