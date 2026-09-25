4bc31ce97a9d45bff1bab35437ebc64860aefac1
brendonguedes29 <bsg181818@gmail.com>
2026-09-25 14:47:12 -0300

    Delete gestao directory

--- gestao/migrations/0007_aprendizagem_prazos.py
+++
@@ -1,7 +0,0 @@
-from django.db import migrations, models
-
-class Migration(migrations.Migration):
-    dependencies=[('gestao','0006_trilhas_alertas_papeis')]
-    operations=[
-        migrations.AddField(model_name='trilhacolaborador',name='inicio',field=models.DateField(blank=True,null=True)),
-    ]
